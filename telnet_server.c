26d2e7465995a16d7f3c4166b854c9e3d799f9e8
Roman Samgin <rsamgin99@gmail.com>
2020-11-22 11:50:58 +0300

    final

--- telnet_server.c
+++ telnet_server.c
@@ -8,117 +8,93 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <signal.h>
-#include <errno.h>
 
-#define BUFSIZE 4096
-#define ARGSSIZE 4
-#define PORT 34567 
+#define BUFSIZE 1024
 #define ADDR "127.0.0.1"
 
-
 void handle_sigchld(int sig) {
-  while (waitpid(-1, NULL, WNOHANG) > 0) {}
+    while (waitpid(-1, NULL, WNOHANG) > 0) 
+    {     
+    } 
 }
 
-
-int main(int argc, char* argv[])
-{
-    pid_t pid;
+int main(int argc, char* argv[])    {
+    if (argc != 2) {
+        puts("argc != 2");
+        return -1;
+    }
+    
     int pfd[2];
-    int rcvd, msg,len;
-    char buffer[BUFSIZE];
-    memset(buffer,0,BUFSIZE);
-    key_t mqid;
-    int srv_s,cl_s,srv_as,cl_addrlen;
+    pid_t pid;
+    int rcvd, len, cl_addrlen;
+    int srv_s, srv_as;
     struct sockaddr_in srv_addr, cl_addr;
+    char buffer[BUFSIZE];
+    int PORT = atoi(argv[1]);
 
-    srv_s=socket(PF_INET,SOCK_STREAM,0);
-    if (srv_s < 0)
+    srv_s = socket(PF_INET, SOCK_STREAM, 0);
+    if (srv_s < 0) {
         perror("ERROR opening socket");
-
-    if (setsockopt(srv_s, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) < 0)
-        perror("setsockopt(SO_REUSEADDR) failed");
-
-#ifdef SO_REUSEPORT
-    if (setsockopt(srv_s, SOL_SOCKET, SO_REUSEPORT, &(int){1}, sizeof(int)) < 0)
-        perror("setsockopt(SO_REUSEPORT) failed");
-#endif
-
-    bzero(&srv_addr,sizeof(srv_addr));
-    srv_addr.sin_family=AF_INET;
-    srv_addr.sin_port=htons(PORT);
-    inet_aton(ADDR, &srv_addr.sin_addr);
-   
-    if(bind(srv_s,(struct sockaddr*)&srv_addr,sizeof(srv_addr))){
-        perror("bind() failed");
         exit(0);
-    };
+    }   
+    srv_addr.sin_family = AF_INET;
+    srv_addr.sin_port = htons(PORT);
+    inet_aton(ADDR, &srv_addr.sin_addr);
+    len = sizeof(srv_addr);
+
+    while (bind(srv_s, (struct sockaddr *)&srv_addr, len) < 0)  {
+        puts("Port already in use, please choose another port: ");
+        scanf("%i", &PORT);
+        srv_addr.sin_port = htons(PORT);
+    }
 
     listen(srv_s, 10);
-
-    while(srv_as=accept(srv_s,(struct sockaddr*)&cl_addr,&len)){
+    puts("Listening...");
+    while(srv_as = accept(srv_s, (struct sockaddr*)&cl_addr, &len)){
+        printf("Client connected\nIP:\t%s\nPort:\t%i\n", inet_ntoa(cl_addr.sin_addr), cl_addr.sin_port);
+        signal(SIGCHLD, &handle_sigchld);
         pid = fork();
-
-        signal(SIGCHLD,&handle_sigchld);
-
-        if (pid==0) {
-            while((rcvd=recvfrom(srv_as,buffer,BUFSIZE,0,(struct sockaddr *)&cl_addr,&cl_addrlen))>0){
-                buffer[rcvd - 2] = '\0';
-                char ** args = (char**)malloc(ARGSSIZE * sizeof(char*));
-                args[0] = "/bin/sh";
-                args[1] = "-c";
-                args[2] = buffer;
-                args[3] = NULL;
-
+        if (pid == 0) {
+            while((rcvd = recvfrom(srv_as, buffer, BUFSIZE, 0, (struct sockaddr *)&cl_addr,&cl_addrlen)) > 0){
+                buffer[rcvd - 2] = '\0';        // remove \n\r
+                char* cmd[] = {"/bin/sh", "-c", buffer, NULL};
                 pipe(pfd);
-                pid=fork();
-
+                pid = fork();
                 if (pid == -1) {
                     perror("Can't fork");
                     exit(-1);
                 }
-                else
-                if(pid>0){
-                    wait(NULL);
-                    close(pfd[1]);
-                    puts("Received:");
-                    memset(buffer,0,BUFSIZE);
- 		    while((rcvd=read(pfd[0],buffer,BUFSIZE))>0){
-                        write(1,buffer,rcvd);
-                        send(srv_as,buffer,rcvd, MSG_NOSIGNAL);
-                        memset(buffer,0,BUFSIZE);
-                    }
-
-                    write(srv_as,"\r", 1);
-
-                    puts("");
-                    close(pfd[0]);
-                }
-                else {
+                else if (pid == 0) {
                     close(pfd[0]);
                     close(STDOUT_FILENO);
                     close(STDERR_FILENO);
                     dup2(pfd[1], STDOUT_FILENO);
                     dup2(pfd[1], STDERR_FILENO);
-                    execvp(args[0],args);
+                    execvp(cmd[0], cmd);
                     close(pfd[1]);
                     exit(0);
                 }
-
+                else if (pid > 0){
+                    wait(NULL);
+                    close(pfd[1]);
+ 		            while((rcvd = read(pfd[0], buffer, BUFSIZE)) > 0){
+                        send(srv_as, buffer, rcvd, MSG_NOSIGNAL);
+                        memset(buffer, '\0', BUFSIZE);
+                    }
+                    write(srv_as,"\r", 1);
+                    close(pfd[0]);
+                }
             }
             close(srv_as);
             exit(0);
-        } else if (pid>0) {
-            //wait(NULL);
-            //waitpid(-1, NULL, WNOHANG);
+        } else if (pid > 0) {
+            wait(NULL);
             close(srv_as);
         } else {
             perror("fork() failed");
         }
     }
 
-    //Cleanup
-    shutdown(srv_s,SHUT_RDWR);
     close(srv_s);
     return 0;
 }
